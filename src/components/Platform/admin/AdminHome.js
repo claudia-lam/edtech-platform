daa4c84757026c3f0924878095537298c995e0e7
Claudia Lam <claudiaslam@gmail.com>
2023-10-05 11:20:49 -0700

    add sidebar items

--- src/components/Platform/admin/AdminHome.js
+++ src/components/Platform/admin/AdminHome.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
 
 function AdminHome() {
+
+  function showSideBar(event) {
+    event.preventDefault();
+  }
+
   return <div>
-    the admin page
+    <Menu>
+      <a id="home" className="menu-item" href="/">Home</a>
+      <a id="about" className="menu-item" href="/about">About</a>
+      <a id="contact" className="menu-item" href="/contact">Contact</a>
+      <a onClick={showSideBar} className="menu-item--small" href="/admin">Settings</a>
+    </Menu>
   </div>;
 }
 
