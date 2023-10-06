d8b0ff8168f0adc3d768e46dce068334a0cb53d9
Claudia Lam <claudiaslam@gmail.com>
2023-10-06 02:04:01 -0700

    create and render students table

--- src/components/Platform/admin/ManageStudents.js
+++ src/components/Platform/admin/ManageStudents.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import StudentList from '../students/StudentList';
 
 function ManageStudents() {
   return (
     <>
       <h1>Manage Students</h1>
+      <div>A search bar</div>
+      <button>Add Student</button>
+      <StudentList />
     </>
   );
 
---
+++ src/components/Platform/students/StudentList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './Student.module.css';
+import { Upload, PencilSquare } from 'react-bootstrap-icons';
+
+function StudentList() {
+  return <div>
+    <table className={styles.studentTable}>
+      <thead>
+        <tr className={styles.studentTableCell}>
+          <th>Student First</th>
+          <th>Student Last</th>
+          <th>Class Type</th>
+          <th>Class</th>
+          <th>Meeting Day</th>
+          <th>Meeting Time(PST)</th>
+          <th>Report</th>
+          <th>Edit</th>
+        </tr>
+        <tr className={styles.studentTableCell}>
+          <td>Taylor</td>
+          <td>Swift</td>
+          <td>Music</td>
+          <td>Vocal</td>
+          <td>Wednesday(Need date?)</td>
+          <td>10:00 AM?</td>
+          <td><Upload /></td>
+          <td><PencilSquare /></td>
+        </tr>
+        <tr className={styles.studentTableCell}>
+          <td>Justin</td>
+          <td>Bieber</td>
+          <td>Language</td>
+          <td>Spanish</td>
+          <td>Monday(Need date?)</td>
+          <td>3:00 PM?</td>
+          <td><Upload /></td>
+          <td><PencilSquare /></td>
+        </tr>
+      </thead>
+    </table>
+  </div>;
+}
+
+export default StudentList;
