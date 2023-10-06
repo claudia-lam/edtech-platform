8c7a6282c9d407ad3e47614e3615fc7db949cada
Claudia Lam <claudiaslam@gmail.com>
2023-10-05 17:05:26 -0700

    change all a tags to NavLinks

--- src/components/Platform/admin/AdminHome.js
+++ src/components/Platform/admin/AdminHome.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import '../routes-nav/SideBar.css';
+import { NavLink } from 'react-router-dom';
 
 function AdminHome() {
 
-  function showSideBar(event) {
-    event.preventDefault();
-  }
+  // function showSideBar(event) {
+  //   event.preventDefault();
+  // }
 
   return <div>
+    <h1>Admin</h1>
     <Menu>
-      <a id="home" className="menu-item" href="/">Home</a>
-      <a id="about" className="menu-item" href="/about">About</a>
-      <a id="contact" className="menu-item" href="/contact">Contact</a>
-      <a onClick={showSideBar} className="menu-item--small" href="/admin">Settings</a>
+      <NavLink className="menu-item" to="/admin"> Dashboard</NavLink>
+      <NavLink className="menu-item" to="/students"> Students</NavLink>
+      <NavLink className="menu-item" to="/teachers"> Teachers</NavLink>
+      <NavLink className="menu-item" to="/reports"> Meeting Reports</NavLink>
+      <NavLink className="menu-item" to="/billing"> Billing </NavLink>
+      {/* <a onClick={showSideBar} className="menu-item--small" href="/admin">Settings</a> */}
     </Menu>
+    <div>A search bar</div>
+    <br></br>
+    <div>Some stats here...</div>
   </div>;
 }
 
