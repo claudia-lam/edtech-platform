d56b1175ef7b1d0ec94d02d402bed6224eceb1c7
Claudia Lam <claudiaslam@gmail.com>
2023-10-06 02:06:21 -0700

    create and render class list for student homepage

---
+++ src/components/Platform/students/ClassList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './Student.module.css';
+import { FileEarmark } from 'react-bootstrap-icons';
+
+function ClassList() {
+  return <div>
+    <table className={styles.studentTable}>
+      <thead>
+        <tr className={styles.studentTableCell}>
+          <th>Teacher First</th>
+          <th>Teacher Last</th>
+          <th>Class Type</th>
+          <th>Class</th>
+          <th>Meeting Day</th>
+          <th>Meeting Time(PST)</th>
+          <th>Report</th>
+        </tr>
+        <tr className={styles.studentTableCell}>
+          <td>Mother </td>
+          <td>Theresa</td>
+          <td>Humanities</td>
+          <td>Charity</td>
+          <td>Wednesday(Need date?)</td>
+          <td>10:00 AM?</td>
+          <td><FileEarmark /></td>
+        </tr>
+        <tr className={styles.studentTableCell}>
+          <td>Justin</td>
+          <td>Bieber</td>
+          <td>Language</td>
+          <td>Spanish</td>
+          <td>Monday(Need date?)</td>
+          <td>3:00 PM?</td>
+          <td><FileEarmark /></td>
+        </tr>
+      </thead>
+    </table>
+  </div>;
+}
+
+export default ClassList;
--- src/components/Platform/students/StudentHome.js
+++ src/components/Platform/students/StudentHome.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ClassList from './ClassList';
+import { Link } from "react-router-dom";
 
 function StudentHome() {
-  return <div>
-    the student page
-  </div>;
+  return (
+    <>
+      <h1>Student Dashboard</h1>
+      <Link>Edit Profile</Link>
+      <ClassList />
+    </>
+  );
 }
 
 export default StudentHome;
