5a0a0135d12fb7147a8822ec9efc766aef205c7f
Claudia Lam <claudiaslam@gmail.com>
2023-10-06 02:05:26 -0700

    create and render teachers table

--- src/components/Platform/admin/ManageTeachers.js
+++ src/components/Platform/admin/ManageTeachers.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import TeacherList from '../teachers/TeacherList';
 
 function ManageTeachers() {
   return (
     <>
       <h1>Manage Teachers</h1>
+      <div>A search bar</div>
+      <button>Add Teacher</button>
+      <TeacherList />
     </>
   );
 
---
+++ src/components/Platform/teachers/TeacherList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './Teacher.module.css';
+import { PencilSquare } from 'react-bootstrap-icons';
+
+function TeacherList() {
+  return <div>
+    <table className={styles.teacherTable}>
+      <thead>
+        <tr className={styles.teacherTableCell}>
+          <th>Teacher First</th>
+          <th>Teacher Last</th>
+          <th>Meeting ID?(need zoom ID instead?)</th>
+          <th>Phone Number</th>
+          <th>Time Zone</th>
+          <th>Level</th>
+          <th>Edit</th>
+        </tr>
+        <tr className={styles.teacherTableCell}>
+          <td>Mother</td>
+          <td>Theresa</td>
+          <td>123456</td>
+          <td>(573)578-4836</td>
+          <td>PST</td>
+          <td>3</td>
+          <td><PencilSquare /></td>
+        </tr>
+        <tr className={styles.teacherTableCell}>
+          <td>Bill</td>
+          <td>Nye</td>
+          <td>67890</td>
+          <td>(513)578-4830</td>
+          <td>PST</td>
+          <td>10</td>
+          <td><PencilSquare /></td>
+        </tr>
+      </thead>
+    </table>
+  </div>;
+}
+
+export default TeacherList;
